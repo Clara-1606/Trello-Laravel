878073098605c55dc7db0a0dbef929c0a0e0152b
Vesval <clara.vesval@ynov.com>
2020-12-31 18:35:27 +0100

    Barre de navigation

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -44,6 +44,12 @@
                             <x-jet-nav-link href="{{ route('categories.index') }}" :active="request()->routeIs('categories.index')">
                                 {{ __('Catégories') }}
                             </x-jet-nav-link>
+                            <x-jet-nav-link href="{{ route('profile.show') }}" :active="request()->routeIs('profile.show')">
+                                {{ __('Profil') }}
+                            </x-jet-nav-link>
+                            <x-jet-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')">
+                                {{ __('Dashboard') }}
+                            </x-jet-nav-link>
                         </div>
                     </div>
         
